c1c3a0bf1a148a762badf975caa24388622148e2
sheelpatel <sheelu18@email.unc.edu>
2022-05-11 12:11:32 -0400

    latest

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const sslAccepts = ["true", "t", "yes", "y"].map(x => x.toUpperCase());
 
 module.exports = {
     HOST : process.env.HOST,
-    SSL : sslAccepts.includes(process.env.SSL.toUpperCase()) ? true : false,
+    SSL : sslAccepts.includes(process.env.SSL?.toUpperCase()) ? true : false,
     SERVER_PORT : process.env.SERVER_PORT,
     APP_PORT : process.env.APP_PORT,
     DB_PORT : process.env.DB_PORT,
